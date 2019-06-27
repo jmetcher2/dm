576172b67fd28e5b7c7bec5c97924c7882a8b56a
jmetcher <jmetcher@gmail.com>
2019-06-27 16:26:22 +1000

    Changes for gradle 5

--- dmpersistence/src/main/java/au/id/lagod/dm/persistence/xml/XmlMapBasicTypeDescriptor.java
+++ dmpersistence/src/main/java/au/id/lagod/dm/persistence/xml/XmlMapBasicTypeDescriptor.java
@@ -1,5 +1,6 @@
 package au.id.lagod.dm.persistence.xml;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
 
@@ -13,28 +14,36 @@ import au.id.lagod.dm.MapToXmlConverter;
 import au.id.lagod.dm.XmlMap;
 import au.id.lagod.dm.XmlToMapConverter;
 
-public class XmlMapBasicTypeDescriptor extends AbstractTypeDescriptor<XmlMap> {
+public class XmlMapBasicTypeDescriptor<T extends XmlMap> extends AbstractTypeDescriptor<T> {
 	private static final long serialVersionUID = 1L;
 	private String rootElementName;
 	private String namespace;
 	private List<String> whitelistAllowedHtmlElementsList;
+	private Class<T> clazz;
 	
-	public XmlMapBasicTypeDescriptor(List<String> whitelistAllowedHtmlElementsList, String rootElementName, String namespace) {
-        super( XmlMap.class, new MutableMutabilityPlan<XmlMap>() {
+	public XmlMapBasicTypeDescriptor(Class<T> clazz, List<String> whitelistAllowedHtmlElementsList, String rootElementName, String namespace) {
+        super( clazz, new MutableMutabilityPlan<T>() {
             @Override
-            protected XmlMap deepCopyNotNull(XmlMap value) {
-            	XmlMap copy = new XmlMap();
-            	copy.putAll((XmlMap) value);
+            protected T deepCopyNotNull(T value) {
+            	T copy;
+				try {
+					copy = clazz.getConstructor().newInstance();
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+					throw new Error(e);
+				}
+            	copy.putAll(value);
                 return copy;
             }
         });
         this.rootElementName = rootElementName;
         this.namespace = namespace;
         this.whitelistAllowedHtmlElementsList = whitelistAllowedHtmlElementsList;
+        this.clazz = clazz;
     }
 
 	@Override
-	public boolean areEqual(XmlMap one, XmlMap another) {
+	public boolean areEqual(T one, T another) {
 		// To deep clone or deep compare two maps is going to take
 		// just as long as writing it out each time, so we might as well
 		// regard map fields as perpetually dirty
@@ -44,12 +53,12 @@ public class XmlMapBasicTypeDescriptor extends AbstractTypeDescriptor<XmlMap> {
 
 
 	@Override
-    public String toString(XmlMap value) {
+    public String toString(T value) {
 		return toString(value, rootElementName, namespace);
 	}
 	
     @SuppressWarnings("unchecked")
-	public String toString(XmlMap value, String rootElementName, String namespace) {
+	public String toString(T value, String rootElementName, String namespace) {
     	try {
     		if (value == null) 
     			return null;
@@ -60,13 +69,19 @@ public class XmlMapBasicTypeDescriptor extends AbstractTypeDescriptor<XmlMap> {
     }
 
     @Override
-    public XmlMap fromString(String string) {
-		return new XmlToMapConverter(whitelistAllowedHtmlElementsList).convertNodesFromXmlString(string);
+    public T fromString(String string) {
+		XmlMap xm =  new XmlToMapConverter(whitelistAllowedHtmlElementsList).convertNodesFromXmlString(string);
+		try {
+			return clazz.getConstructor(XmlMap.class).newInstance(xm);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new Error(e);
+		}
     }
 
     
 	@SuppressWarnings({"unchecked"})
-    public <X> X unwrap(XmlMap value, Class<X> type, WrapperOptions options) {
+    public <X> X unwrap(T value, Class<X> type, WrapperOptions options) {
         if ( value == null ) {
             return null;
         }
@@ -79,15 +94,15 @@ public class XmlMapBasicTypeDescriptor extends AbstractTypeDescriptor<XmlMap> {
         throw unknownUnwrap( type );
     }
 
-    public <X> XmlMap wrap(X value, WrapperOptions options) {
+    public <X> T wrap(X value, WrapperOptions options) {
         if ( value == null ) {
             return null;
         }
         if ( String.class.isInstance( value ) ) {
             return fromString( (String) value );
         }
-        if ( XmlMap.class.isInstance( value ) ) {
-            return (XmlMap) value;
+        if ( clazz.isInstance( value ) ) {
+            return (T) value;
         }
         throw unknownWrap( value.getClass() );
     }
---
+++ dmpersistence/src/test/java/au/id/lagod/dm/test/BogusTest.java
@@ -0,0 +1,12 @@
+package au.id.lagod.dm.test;
+
+import org.junit.Test;
+
+public class BogusTest {
+	
+	@Test
+	public void bogus() {
+		System.out.println("Easier to create a bogus test than to fix ajc complaining about no test sources");
+	}
+
+}
